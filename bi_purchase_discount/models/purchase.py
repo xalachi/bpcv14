5c613027353b35e870c4a0269b4a3c1814b0a9b2
jhonny16 <tinny.1609@gmail.com>
2021-11-05 09:59:52 -0500

    mejora en cálculo de totales bajo descuentos en órdenes de compra

--- bi_purchase_discount/models/purchase.py
+++ bi_purchase_discount/models/purchase.py
@@ -12,19 +12,26 @@ class purchase_order_line(models.Model):
     @api.depends('product_qty', 'price_unit', 'taxes_id','discount')
     def _compute_amount(self):
         for line in self:
-            taxes = line.taxes_id.compute_all(line.price_unit, line.order_id.currency_id, line.product_qty, product=line.product_id, partner=line.order_id.partner_id)
-            if line.discount:
-                discount = (line.price_unit * line.discount * line.product_qty)/100
-                line.update({
-                    'price_tax': taxes['total_included'] - taxes['total_excluded'],
-                    'price_total': taxes['total_included'] ,
-                    'price_subtotal': taxes['total_excluded'] - discount,
-                })
-            else:
-                line.update({
-                    'price_tax': taxes['total_included'] - taxes['total_excluded'],
-                    'price_total': taxes['total_included'],
-                    'price_subtotal': taxes['total_excluded'],
-                })
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.taxes_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_id)
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
+            # taxes = line.taxes_id.compute_all(line.price_unit, line.order_id.currency_id, line.product_qty, product=line.product_id, partner=line.order_id.partner_id)
+            # if line.discount:
+            #     discount = (line.price_unit * line.discount * line.product_qty)/100
+            #     line.update({
+            #         'price_tax': taxes['total_included'] - taxes['total_excluded'],
+            #         'price_total': taxes['total_included'] ,
+            #         'price_subtotal': taxes['total_excluded'] - discount,
+            #     })
+            # else:
+            #     line.update({
+            #         'price_tax': taxes['total_included'] - taxes['total_excluded'],
+            #         'price_total': taxes['total_included'],
+            #         'price_subtotal': taxes['total_excluded'],
+            #     })
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
